38e6726a0d61d43ac396066eb37f6540eebca3b5
TimonKosgei <timonkosgei15209@gmail.com>
2024-10-13 16:25:30 +0300

    Did labs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+  const form = document.querySelector('form')
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    let id = e.target.searchByID.value
+    fetch(`http://localhost:3000/movies/${id}`)
+    .then(res => res.json())
+    .then(movies => {
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        title.textContent = movies.title;
+        summary.textContent = movies.summary;
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
